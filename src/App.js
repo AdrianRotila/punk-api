e7ac141d8b9bf20184e9cdb682e3e2e9d2314770
Adrian Rotila <adrianrotila@gmail.com>
2022-09-12 17:51:59 +0100

    Project Set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [beers, setBeers] = useState();
+
+  const getBeers = async () => {
+    const url = "https://api.punkapi.com/v2/beers";
+    const res = await fetch(url);
+    const data = await res.json();
+    setBeers(data)
+  }
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { }
+    
       </header>
     </div>
   );
