5411283780e909e2571ef3939f6ad7aa7037bdd2
AdrianRotila <adrianrotila@gmail.com>
2022-09-15 21:48:38 +0100

    sort beers by ibu, brewed year and abv

--- src/containers/BeersContainer/BeersContainer.jsx
+++ src/containers/BeersContainer/BeersContainer.jsx
@@ -6,31 +6,36 @@ import { useState, useEffect } from 'react';
 const BeersContainer = ({searchedTerm, bitterBeers, classicRange, highABV}) => {
     const [beers, setBeers] = useState([]);
 
-    const fetchBeers = async (bitterBeers) => {
+    const fetchBeers = async (bitterBeers, classicRange, highABV) => {
         
-        let urlWithParams = [];
+        let allBeers = [];
 
         for (let index = 1; index < 6; index++) {
             let url = `https://api.punkapi.com/v2/beers?page=${index}&per_page=80`;
             let res = await fetch(url);
             let data = await res.json();
-            Array.prototype.push.apply(urlWithParams, data)
+            Array.prototype.push.apply(allBeers, data)
         }
         
-        setBeers(urlWithParams)
+        setBeers(allBeers)
         
-       
+        if(bitterBeers === true) {setBeers(allBeers.filter((beer) => beer.ibu <= 70 && beer.ibu >= 50))}
+        if(classicRange === true) {setBeers(allBeers.filter((beer) => parseInt(beer.first_brewed.substring(beer.first_brewed.length-4)) > 2010))}
+        if(highABV === true) {setBeers(allBeers.filter((beer) => beer.abv > 6))}
+        
+        
     }
 
     useEffect(() => {
-      fetchBeers(bitterBeers);
-    }, [bitterBeers])
+      fetchBeers(bitterBeers, classicRange, highABV);
+    }, [bitterBeers, classicRange, highABV])
     
     const search = (beers, searchedTerm) => {
         return beers.filter((beer) => {
             return beer.name.toLowerCase().includes(searchedTerm.toLowerCase()) || 
-            beer.ibu.toString().includes(searchedTerm) ||
-            beer.abv.toString().includes(searchedTerm) ||
+            beer.ibu?.toString().includes(searchedTerm) ||
+            beer.abv?.toString().includes(searchedTerm) ||
+            beer.description.toLowerCase().includes(searchedTerm.toLowerCase()) ||
             beer.food_pairing.join(", ").toLowerCase().includes(searchedTerm.toLowerCase())
         } )
     }
